262f0a35b0a8a71e57d090e01db81556c89ba91b
robertkkan2 <106627599+robertkkan2@users.noreply.github.com>
2023-12-06 23:42:31 -0500

    Update index.js

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -17,7 +17,7 @@ function Kanbas() {
 		endDate: "2023-12-15",
 	});
 	const API_BASE = process.env.REACT_APP_API_BASE;
-	const URL = `${API_BASE}/courses`;
+	const URL = `${API_BASE}/api/courses`;
 	const addCourse = async () => {
 		const response = await axios.post(URL, course);
 
